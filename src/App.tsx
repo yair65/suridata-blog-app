f7458fbdfac25744c84097b7ca2795db9a6a4ab8
Yair Feldman <yairfeldman65@gmail.com>
2022-04-21 20:01:36 +0300

    init eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
+function App () {
   const [count, setCount] = useState(0)
 
   return (
